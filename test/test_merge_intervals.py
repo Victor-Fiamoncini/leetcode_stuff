6c8a7a429dacec000f5ec951c8c073014e047ba7
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2024-11-17 18:09:32 -0300

    test: adds merge-intervals unit test

---
+++ test/test_merge_intervals.py
@@ -0,0 +1,9 @@
+from src.merge_intervals import merge_intervals
+
+
+def test_merge_intervals() -> None:
+    assert merge_intervals([[1, 3], [2, 6], [8, 10], [15, 18]]) == [
+        [1, 6],
+        [8, 10],
+        [15, 18],
+    ]
