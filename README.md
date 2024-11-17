4e7923ea25f5fcc272abfe51d3b8c1c9b3e4e2e5
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2024-11-16 22:04:03 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Leetcode Stuff
+
+Some solved leetcode problems.
+
+## Tools 🛠
+
+- Python v3.11.6
+
+Released in 2024. This project is under the MIT license.
+
+By [Victor B. Fiamoncini](https://github.com/Victor-Fiamoncini) ☕️
---
+++ src/two_sum.py
@@ -0,0 +1,43 @@
+"""
+Given an array of integers and a target, find two numbers such that they add up to the target.
+"""
+
+
+def two_sum(items: list[int], target: int) -> list[int]:
+    # Brute force approach
+    # for i in range(len(items)):
+    #     for j in range(i + 1, len(items)):
+    #         if items[i] + items[j] == target:
+    #             return [i, j]
+
+    # Hashmap approach
+    # visited = {}
+
+    # for index, item in enumerate(items):
+    #     complement = target - item
+
+    #     if complement in visited:
+    #         return [visited[complement], index]
+
+    #     visited[item] = index
+
+    # return []
+
+    # Two pointers approach
+    start = 0
+    end = len(items) - 1
+
+    for _ in range(len(items)):
+        sum = items[start] + items[end]
+
+        if sum < target:
+            start = start + 1
+        elif sum > target:
+            end = end - 1
+        else:
+            return [start, end]
+
+    return []
+
+
+print(two_sum([1, 2, 3, 4, 5, 6, 7, 8], 5))
